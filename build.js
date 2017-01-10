96b1f1ac85707c133a3d947f66d89a8caf8b16a8
Clyde Smets <clyde.smets@gmail.com>
2017-01-10 21:32:13 +1000

    Updated build script

--- build.js
+++ build.js
@@ -1,1 +1,26 @@
-console.log("hello")
+const csscrypt = require('csscrypt')
+const cheerio = require('cheerio')
+const fs = require('fs')
+
+const file = fs.readFileSync('index.html')
+
+const $ = cheerio.load(file)
+
+const body = $('body').html()
+
+const options = {
+    pad: "=",
+    encoding: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/",
+    size: 6,
+    key: '3924834902384'
+}
+const encryptedBody = csscrypt.encrypt(body, options)
+const decrypted = csscrypt.decrypt(encryptedBody, options)
+
+$('body').html(encryptedBody)
+
+const html = $.html()
+
+fs.writeFileSync('out.html',html,'utf8')
+
+console.log(decrypted)
